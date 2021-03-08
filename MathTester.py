6aae4b1bee1b6d3d76b16beec7e0e82aae90890b
DevBubo <bubak.filip@gmail.com>
2021-03-08 23:13:05 +0100

    added adding

---
+++ MathTester.py
@@ -0,0 +1,19 @@
+from time import time
+from random import randint
+from sys import stdout, stdin
+
+
+def printf(text, divider="\n"): #faster print
+    text = str(text)
+    stdout.write(text + divider)
+
+
+def input(prompt): #faster input
+    stdout.write(prompt)
+    return stdin.readline()
+
+def adding(start=10000, end=100000):
+    num1 = randint(start, end)
+    num2 = randint(start, end)
+    printf(f"{num1} + {num2} = ")
+    return num1 + num2
