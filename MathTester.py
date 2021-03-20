b1481eea6638f8d89bb739f937b06c4e71753092
DevBubo <bubak.filip@gmail.com>
2021-03-20 17:18:59 +0100

    Generalized equations

--- MathTester.py
+++ MathTester.py
@@ -8,9 +8,9 @@ def printf(text, divider="\n"): #faster print
     stdout.write(text + divider)
 
 
-def input(prompt): #faster input
+def input(prompt="\n"): #faster input
     stdout.write(prompt)
-    return stdin.readline()
+    return stdin.readline().strip()
 
 
 def adding(start=10000, end=100000):
@@ -39,3 +39,46 @@ def dividing(start=1000, end=10000):
     num2 = randint(start, end)
     printf(f"{max(num1, num2)} / {min(num1, num2)} = ", "")
     return num1 / num2
+
+defaults = {"adding": ("+", 10000, 100000), "subtracting": ("-", 10000, 100000), "multiplying": ("*", 10, 100), "dividing": ("/", 1000, 10000)} #command: (operator, start, end)
+def calculating(command, start=None, end=None):
+    if start == None or end == None:
+        start = defaults[command][1]
+        end = defaults[command][2]
+    num1 = randint(start, end)
+    num2 = randint(start, end)
+    if command in ("subtracting", "dividing"): #In case we get negative or number between 0 and 1
+        num1, num2 = max(num1, num2), min(num1, num2)
+    printf(f"{num1} {defaults[command][0]} {num2} = ", "")
+    return round(eval(f"{num1} {defaults[command][0]} {num2}"), 2)
+
+
+calculating("adding")
+def helping():
+    print("""
+Created for people, who want to calculate fast, and without a calculator. @DevBubo
+Commands:
+adding
+subtracting
+multiplying
+dividing
+
+You can exit of any mode by typing "Cancel".
+You can also edit the range of numbers, that will appear in your calculation by typing a command,
+you want and adding range, e.g "add 50000 5000000"
+If you want this message to reappear, type "cancel".
+""")
+
+
+command = "cancel"
+command_stopper = False
+while True:
+    if command == "cancel": #pops up help menu
+        helping()
+        command = input("What would you like to do? ")
+    else:
+        if command in defaults:
+            result = calculating(command)  #prints equation, and saves its result to result
+            yourResult = float(input("Enter your result: "))
+            while yourResult not in (result, "cancel"):
+                yourResult = float(input("Enter your result: "))
